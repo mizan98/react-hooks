380ef3dc0febe673f4d9103a7b3a8e40da07a00f
Mizan <mizan_98@hotmail.co.uk>
2019-11-05 17:21:11 +0000

    initiail commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState(
+    [
+      {
+        name: "Harry Potter",
+        mainActor: "Daniel Radcliffe",
+        yearOfRelease: 2002
+      },
+      {
+        name: "Lord Of the Rings",
+        mainActor: "Elijah Woods",
+        yearOfRelease: 2001
+      }
+    ]
+  )
+
+  const changeMovie = (newName) => {                   //what if i want to change the name of the first arrauy without having to copy all of the array?
+    let newState = [...movies]                            //'...' used to repeat previous array witout typing it
+    newState[0].name = "Inception"
+    setMovies(newState)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {movies.map( (movie)=> {
+        return <h1> Film: {movie.name} released on: {movie.yearOfRelease}</h1>
+      })}
+      <button onClick={changeMovie}>Change Movie</button>
     </div>
-  );
+  )
 }
+
 
 export default App;
